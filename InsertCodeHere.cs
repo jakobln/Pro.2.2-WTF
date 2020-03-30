1f492ff86b5aef0c6d7a701478b279d0ef972311
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:58:24 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WTF
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            MysticNumbers numbers1 = new MysticNumbers();
+            int result1 = numbers1.ThreeNumbers(5, 10, 3);
+            System.Console.WriteLine($"The highest number is {result1}");
+
+            MysticNumbers numbers2 = new MysticNumbers();
+            int result2 = numbers2.TwoNumbers(11, 10);
+            System.Console.WriteLine($"The highest number is {result2}");
+
+            MysticNumbers numbers3 = new MysticNumbers();
+            int result = numbers3.FourNumbers(11, 12, 13, 14);
+            System.Console.WriteLine($"The highest number is {result}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ MysticNumbers.cs
@@ -0,0 +1,52 @@
+﻿namespace WTF
+{
+    public class MysticNumbers
+    {
+        #region Methods
+        public int ThreeNumbers(int a, int b, int c)
+        {
+            return TwoNumbers(TwoNumbers(a, b),c);
+
+            /*int result1;
+
+            if (b > a)
+            {
+                result1 = b;
+                if (c > b)
+                {
+                    result1 = c;
+                }
+            }
+            else
+            {
+                result1 = a;
+                if (c > a)
+                {
+                    result1 = c;
+                }
+            }
+
+            return result1;*/
+        } 
+        public int TwoNumbers(int a, int b)
+        {
+            int result2;
+
+            if (b > a)
+            {
+                result2 = b;
+            }
+            else
+            {
+                result2 = a;
+            }
+
+            return result2;
+        }
+        public int FourNumbers(int a, int b, int c, int d)
+        {
+            return TwoNumbers(TwoNumbers(a,b),TwoNumbers(c,d));
+        }
+        #endregion
+    }
+}
